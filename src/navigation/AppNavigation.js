98708fb1a7e27d724c86080338ad7ac9fef2d0ad
Victor M Moreira <109500795+Pir4te64@users.noreply.github.com>
2025-01-29 10:05:48 -0300

    tabs

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -4,17 +4,42 @@ import { FavouritesScreen } from "../screens/FavouritesScreen";
 import { RankingScreen } from "../screens/RankingScreen";
 import { SearchScreen } from "../screens/SearchScreen";
 import { AccountScreen } from "../screens/AccountScreen";
-
+import { Icon } from "react-native-elements";
+import { screen } from "../utils/index";
 const Tab = createBottomTabNavigator();
 
 export function AppNavigation() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Restaurant" component={RestaurantsScreen} />
-      <Tab.Screen name="Favourites" component={FavouritesScreen} />
-      <Tab.Screen name="Ranking" component={RankingScreen} />
-      <Tab.Screen name="Search" component={SearchScreen} />
-      <Tab.Screen name="Account" component={AccountScreen} />
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: "#00a680",
+        tabBarInactiveTintColor: "#646464",
+        tabBarIcon: ({ color, size }) => screenOptions(route, color, size),
+      })}
+    >
+      <Tab.Screen name={screen.restaurant.tab} component={RestaurantsScreen} />
+      <Tab.Screen name={screen.favourites.tab} component={FavouritesScreen} />
+      <Tab.Screen name={screen.ranking.tab} component={RankingScreen} />
+      <Tab.Screen name={screen.search.tab} component={SearchScreen} />
+      <Tab.Screen name={screen.account.tab} component={AccountScreen} />
     </Tab.Navigator>
+  );
+}
+
+function screenOptions(route, color, size) {
+  let iconName;
+  if (route.name === screen.restaurant.tab) {
+    iconName = "compass-outline";
+  } else if (route.name === screen.favourites.tab) {
+    iconName = "heart-outline";
+  } else if (route.name === screen.ranking.tab) {
+    iconName = "star-outline";
+  } else if (route.name === screen.search.tab) {
+    iconName = "magnify";
+  } else if (route.name === screen.account.tab) {
+    iconName = "home-outline";
+  }
+  return (
+    <Icon type="material-community" name={iconName} size={size} color={color} />
   );
 }
---
+++ src/utils/screenName.js
@@ -0,0 +1,23 @@
+const restaurantStack = {
+  tab: "Restaurant",
+};
+const favouritesStack = {
+  tab: "Favourites",
+};
+const rankingStack = {
+  tab: "Ranking",
+};
+const searchStack = {
+  tab: "Search",
+};
+const accountStack = {
+  tab: "Account",
+};
+
+export const screen = {
+  restaurant: restaurantStack,
+  favourites: favouritesStack,
+  ranking: rankingStack,
+  search: searchStack,
+  account: accountStack,
+};
