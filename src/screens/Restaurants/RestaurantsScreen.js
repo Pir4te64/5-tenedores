0ca13a98602a11ab031999cf3853c75ccdc79419
Victor M Moreira <109500795+Pir4te64@users.noreply.github.com>
2025-01-29 10:48:51 -0300

    add stack

--- src/screens/Restaurants/RestaurantsScreen.js
+++ src/screens/Restaurants/RestaurantsScreen.js
@@ -1,10 +1,16 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { View, Text } from "react-native";
-
+import { Button } from "react-native-elements";
+import { screen } from "../../utils";
 export function RestaurantsScreen() {
+  const navigate = useNavigation();
+  const goToRestaurant = () => {
+    navigate.navigate(screen.restaurant.addRestaurant);
+  };
   return (
     <View>
-      <Text>Restaurants...</Text>
+      <Button title={"Crear restaurant"} onPress={goToRestaurant} />
     </View>
   );
 }
